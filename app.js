d6d9ac4604f55b3a27d15dca595b2c48f1c6ddfe
HannahGuo <hguo2102@gmail.com>
2020-05-31 07:22:16 -0400

    wow i just made this so much better, still have to fix filtering + multiple albums but wow, this is so much better

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
   var request = require('request');
   var http = require("http");
-  var artistID = "3Nrfpe0tUJi4K4DXYWgMUX";
+  var artistID = "47mIJdHORyRerp4os813jD";
   var artistName = "";
-  var albumID = [];
-  var trackID = [];
   var trackNameAndView = {
     "items": []
   };
@@ -19,19 +17,42 @@
     json: true
   };
 
-  function makeCustomTrackFormat(trackName, trackPopularity) {
-    return {
-      name: trackName,
-      popularity: trackPopularity
-    };
+  function makeCustomTrackFormat(trackInfo) {
+    var idSTR = "";
+    trackInfo.forEach(function (item, i) {
+      if (!checkSongs(item.name)) idSTR += item.id + ",";
+    });
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var token = body.access_token;
+        var options = {
+          url: `https://api.spotify.com/v1/tracks/?ids=${idSTR.slice(0, -1)}`,
+          headers: {
+            'Authorization': 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            body.tracks.forEach(function (item, i) {
+
+              if (!(trackNameAndView.items.some(myItem => myItem.name == item.name))) {
+                trackNameAndView.items.push({
+                  name: item.name,
+                  popularity: item.popularity
+                });
+              }
+            });
+          }
+        });
+      }
+    });
   }
 
-  function chunkID(arr, chunkSize) {
-    // Taken directly from https://stackoverflow.com/questions/8495687/split-array-into-chunks
-    var R = [];
-    for (var i = 0, len = arr.length; i < len; i += chunkSize)
-      R.push(arr.slice(i, i + chunkSize));
-    return R;
+  function checkSongs(track) {
+    var song = track.toString()
+    return song.includes("Commentary") || song.includes("Karaoke") || song.includes("Voice Memo");
   }
 
   function getArtist() {
@@ -55,11 +76,42 @@
   }
 
   function getAlbums() {
+    var offset = 0;
     request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         var token = body.access_token;
         var options = {
-          url: `https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album,single,appears_on,compilation&limit=50`,
+          url: `https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album,single,compilation&limit=50&offset=${50 * offset}`,
+          headers: {
+            'Authorization': 'Bearer ' + token
+          },
+          json: true
+        };
+
+        request.get(options, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            body.items.forEach(function (item, i) {
+              getAlbumTracks(item.id);
+            });
+          } else {
+            console.log("Get Album Error: " + error + " " + JSON.stringify(response, null, 4));
+          }
+        });
+
+      } else {
+        console.log("Post Albums Error: " + error + " " + JSON.stringify(response, null, 4));
+      }
+    });
+  }
+
+  var albumTracksIDs = [];
+
+  function getAlbumTracks(aID) {
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var token = body.access_token;
+        var options = {
+          url: `https://api.spotify.com/v1/albums/${aID}/tracks?limit=50`,
           headers: {
             'Authorization': 'Bearer ' + token
           },
@@ -67,88 +119,24 @@
         };
         request.get(options, function (error, response, body) {
           if (!error && response.statusCode === 200) {
-            for (let i = 0; i < body.items.length; i++) {
-              albumID.push(body.items[i].id);
+            body.items.forEach(function (item, i) {
+              albumTracksIDs.push({
+                name: item.name,
+                id: item.id
+              });
+            });
+            if (albumTracksIDs.length > 35) {
+              makeCustomTrackFormat(albumTracksIDs);
+              albumTracksIDs = [];
             }
-            setTimeout(function () {
-              getAlbumTracks();
-            }, 8000);
           } else {
-            console.log("Get Album Error: " + error + " " + JSON.stringify(response, null, 4));
+            console.log("Get Album Tracks Error: " + error + " " + JSON.stringify(response, null, 4));
           }
         });
-      } else {
-        console.log("Post Albums Error: " + error + " " + JSON.stringify(response, null, 4));
-      }
-    });
-  }
-
-  function getAlbumTracks() {
-    request.post(authOptions, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var token = body.access_token;
-        for (var aID = 0; aID < albumID.length; aID++) {
-          var options = {
-            url: `https://api.spotify.com/v1/albums/${albumID[aID]}/tracks?limit=50`,
-            headers: {
-              'Authorization': 'Bearer ' + token
-            },
-            json: true
-          };
-          request.get(options, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-              body.items.forEach(function (item, i) {
-                trackID.push(item.id);
-              });
-              getTrackNameAndPopularity();
-              trackID = [];
-            } else {
-              console.log("Get Album Tracks Error: " + error + " " + JSON.stringify(response, null, 4));
-            }
-          });
-        }
       } else {
         console.log("Post Album Tracks Error: " + error + " " + JSON.stringify(response, null, 4));
       }
     });
-  }
-
-  function getTrackNameAndPopularity() {
-    var groupedTrackIds = chunkID(trackID, 49);
-    request.post(authOptions, function (error, response, body) {
-      for (var tID = 0; tID < 1; tID++) {
-        if (!error && response.statusCode === 200) {
-          var token = body.access_token;
-          var options = {
-            url: `https://api.spotify.com/v1/tracks/?ids=${groupedTrackIds[tID].join(",")}`,
-            headers: {
-              'Authorization': 'Bearer ' + token
-            },
-            json: true
-          };
-
-          request.get(options, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-              for (var i = 0; i < body.tracks.length; i++) {
-                var trackName = body.tracks[i].name;
-                var trackPopularity = body.tracks[i].popularity;
-
-                if (!(trackNameAndView.items.some(item => item.name == trackName)))
-                  trackNameAndView.items.push(makeCustomTrackFormat(trackName, trackPopularity));
-              }
-            } else {
-              console.log("Get Track Names and Pop Error: " + error + " " + JSON.stringify(response, null, 4));
-            }
-          });
-        } else {
-          console.log("Post Track Name and Pop Error: " + error + " " + JSON.stringify(response, null, 4));
-        }
-      }
-    });
-  }
-
-  function sleep(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
   }
 
   getAlbums();
@@ -157,8 +145,10 @@
 
   http.createServer(function (request, response) {
     response.writeHead(200, {
-      "Content-Type": "text/html; charset=utf-8"
+      "Content-Type": "text/html; charset=utf-8; no-cache"
     });
+
+    trackNameAndView.items.flat();
 
     trackNameAndView.items.sort(function (a, b) {
       return b.popularity - a.popularity
@@ -170,6 +160,7 @@
     for (var i = 0; i < trackNameAndView.items.length; i++) {
       response.write(`<span id="testID"> ${i + 1} - Popularity Score ${trackNameAndView.items[i].popularity} - ${trackNameAndView.items[i].name}</span><br>`);
     }
+
     response.end();
 
   }).listen(8888);
